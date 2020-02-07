9035de8ca64c7dbc99eb923ebb07d118d82ef963
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2020-02-07 18:57:01 +0100

    BoardTest: new test 'put two combinations' added.

--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -3,9 +3,22 @@ import org.scalatest.FunSuite
 
 class BoardTest extends FunSuite {
 
-    test("put on 1,1 with x") {
+    test("put one combination") {
       val board = new Board()
-      val combination = new Combination(List('R', 'R', 'R','R'));
-      assert(board.put(combination) == new Board(List(), List(new Combination(List('R', 'R', 'R','R')))));
+      val combination = new Combination(List('R','R','R','R'));
+      assert(board.put(combination) == new Board(List(), List(new Combination(List('R','R','R','R')))));
     }
+
+  test("put two combinations") {
+    val board = new Board()
+    val combination1 = new Combination(List('R','R','R','R'));
+    val combination2 = new Combination(List('B','B','B','B'));
+
+    assert(board.put(combination1).put(combination2) ==
+      new Board(List(),
+        List(
+          new Combination(List('R','R','R','R')),
+          new Combination(List('B','B','B','B')
+        ))));
+  }
 }

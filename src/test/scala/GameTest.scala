d5fe8bb94073dd276b216a0be2ece47c07f12129
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2020-02-07 23:48:49 +0100

    GameTest: isWinner, isComplete unit tests added

--- src/test/scala/GameTest.scala
+++ src/test/scala/GameTest.scala
@@ -3,15 +3,37 @@ import org.scalatest.FunSuite
 
 class GameTest extends FunSuite {
 
+  val secret: List[Char] = List('B','R','Y','R')
+  val winner: List[Char] = secret
+  val looser: List[Char] = secret.tail :+ 'G'
+
   test("Propose combination") {
     val game = new Game()
-    val combination = new Combination(List('B','R','Y','R'));
-    assert(game.propose(combination) == new Game(new Board(List(), List(new Combination(List('B','R','Y','R'))))));
+    val combination = new Combination(looser);
+    assert(game.propose(combination) == new Game(new Board(secret, List(new Combination(looser)))));
+  }
+
+  test("Is not winner") {
+    val combination = new Combination(looser);
+    val game = new Game(new Board(secret)).propose(combination);
+    assert(!game.isWinner);
   }
 
   test("Is winner") {
-    val combination = new Combination(List('B','R','Y','R'));
-    val game = new Game(new Board(List('B','R','Y','R'))).propose(combination);
-    assert(game.isWinner() == true);
+    val combination = new Combination(winner);
+    val game = new Game(new Board(secret)).propose(combination);
+    assert(game.isWinner);
+  }
+
+  test("Is not complete") {
+    val combination = new Combination(looser);
+    val game = new Game(new Board(secret)).propose(combination);
+    assert(!game.isComplete);
+  }
+
+  test("Is complete") {
+    val combination = new Combination(looser);
+    val game = new Game(new Board(secret)).propose(combination).propose(combination).propose(combination);
+    assert(game.isComplete);
   }
 }
